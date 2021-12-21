f53cd49f282000146214f573cbff2dcb99d863ae
kgx442 <Skirmantas.Sinkus@teliacompany.com>
2021-12-21 10:36:06 +0200

    test

--- src/slack-send.js
+++ src/slack-send.js
@@ -41,14 +41,7 @@ module.exports = async function slackSend(core) {
     if (typeof botToken !== 'undefined' && botToken.length > 0) {
       const message = core.getInput('slack-message');
       const channelId = core.getInput('channel-id');
-      let web;
-      const proxy = process.env.https_proxy;
-      if (proxy !== 'undefined' && proxy.length > 0) {
-        const proxyAgent = new HttpsProxyAgent(proxy);
-        web = new WebClient(botToken, { agent: proxyAgent });
-      } else {
-        web = new WebClient(botToken);
-      }
+      const web = new WebClient(botToken);
 
       if (channelId.length > 0 && (message.length > 0 || payload)) {
         // post message
